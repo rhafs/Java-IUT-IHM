c8cfb57bd5b8ed7f6c58391ceed7197f81252e61
Gabriel Augendre <gabriel@augendre.info>
2015-04-09 12:00:22 +0200

    Solve calculatrice.

---
+++ src/main/java/calculatrice/Calculatrice.java
@@ -0,0 +1,32 @@
+package calculatrice;
+
+import javax.swing.*;
+
+/**
+ * Created on 09/04/15.
+ *
+ * @author gaugendre
+ */
+public class Calculatrice extends JFrame{
+    private JPanel mainPanel;
+
+    public Calculatrice() {
+        super("Calculatrice");
+        this.mainPanel = new MainPanel().getMainPanel();
+        this.setContentPane(mainPanel);
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.pack();
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(
+                    UIManager.getSystemLookAndFeelClassName());
+        } catch (UnsupportedLookAndFeelException | ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+            e.printStackTrace(System.err);
+        }
+        new Calculatrice();
+    }
+}
