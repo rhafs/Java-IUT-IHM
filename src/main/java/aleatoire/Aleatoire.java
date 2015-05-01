bcf098c426d3cbaaf78f7a8c49e9a356b5d0d9d8
Gabriel Augendre <gabriel@augendre.info>
2015-05-01 17:54:13 +0200

    Add random integer generator.

---
+++ src/main/java/aleatoire/Aleatoire.java
@@ -0,0 +1,32 @@
+package aleatoire;
+
+import javax.swing.*;
+
+/**
+ * Created on 01/05/15.
+ *
+ * @author gaugendre
+ */
+public class Aleatoire extends JFrame {
+    private JPanel mainPanel;
+
+    public Aleatoire() {
+        super("Génération automatique d'un entier aléatoire");
+        this.mainPanel = new MainPanel(this).getMainPanel();
+        this.setContentPane(mainPanel);
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.pack();
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(
+                    UIManager.getSystemLookAndFeelClassName());
+        } catch (UnsupportedLookAndFeelException | ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+            e.printStackTrace(System.err);
+        }
+        new Aleatoire();
+    }
+}
