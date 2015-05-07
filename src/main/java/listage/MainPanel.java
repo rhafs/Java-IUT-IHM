1897f7d4d96664f34b7c41fca234718043282244
Gabriel Augendre <gabriel@augendre.info>
2015-05-07 11:03:56 +0200

    Working example.

--- src/main/java/listage/MainPanel.java
+++ src/main/java/listage/MainPanel.java
@@ -2,6 +2,7 @@ package listage;
 
 import com.intellij.uiDesigner.core.GridConstraints;
 import com.intellij.uiDesigner.core.GridLayoutManager;
+import com.intellij.uiDesigner.core.Spacer;
 
 import javax.swing.*;
 import java.awt.*;
@@ -14,10 +15,25 @@ import java.awt.*;
 public class MainPanel {
     private JPanel mainPanel;
     private JList list1;
+    private JTextField textField1;
     private JFrame container;
+    private DefaultListModel<String> listModel;
 
     public MainPanel(JFrame container) {
         this.container = container;
+        this.listModel = new DefaultListModel<>();
+        list1.setModel(listModel);
+        String[] listeFruits = {"pomme", "kiwi", "poire", "ananas"};
+        for (String fruit : listeFruits) {
+            listModel.addElement(fruit);
+        }
+
+        list1.setSelectedIndex(0);
+        list1.addListSelectionListener(e -> {
+            if (!list1.isSelectionEmpty()) {
+                textField1.setText(list1.getSelectedValue().toString());
+            }
+        });
     }
 
     public JPanel getMainPanel() {
@@ -40,9 +56,24 @@ public class MainPanel {
      */
     private void $$$setupUI$$$() {
         mainPanel = new JPanel();
-        mainPanel.setLayout(new GridLayoutManager(1, 1, new Insets(0, 0, 0, 0), -1, -1));
+        mainPanel.setLayout(new GridLayoutManager(4, 3, new Insets(0, 0, 0, 0), -1, -1));
+        final JScrollPane scrollPane1 = new JScrollPane();
+        mainPanel.add(scrollPane1, new GridConstraints(1, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, null, new Dimension(300, -1), null, 0, false));
+        scrollPane1.setBorder(BorderFactory.createTitledBorder("Bonjour"));
         list1 = new JList();
-        mainPanel.add(list1, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_WANT_GROW, null, new Dimension(150, 50), null, 0, false));
+        list1.setFont(new Font("Monospaced", list1.getFont().getStyle(), 16));
+        list1.setSelectionMode(0);
+        scrollPane1.setViewportView(list1);
+        final Spacer spacer1 = new Spacer();
+        mainPanel.add(spacer1, new GridConstraints(1, 2, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, new Dimension(10, -1), null, 0, false));
+        final Spacer spacer2 = new Spacer();
+        mainPanel.add(spacer2, new GridConstraints(1, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, new Dimension(10, -1), null, 0, false));
+        final Spacer spacer3 = new Spacer();
+        mainPanel.add(spacer3, new GridConstraints(3, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_VERTICAL, 1, GridConstraints.SIZEPOLICY_WANT_GROW, null, new Dimension(-1, 10), null, 0, false));
+        final Spacer spacer4 = new Spacer();
+        mainPanel.add(spacer4, new GridConstraints(0, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_VERTICAL, 1, GridConstraints.SIZEPOLICY_WANT_GROW, null, new Dimension(-1, 10), null, 0, false));
+        textField1 = new JTextField();
+        mainPanel.add(textField1, new GridConstraints(2, 1, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, -1), null, 0, false));
     }
 
     /**
