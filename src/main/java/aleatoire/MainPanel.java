2a45e824eb3bb084a08c2f7a5633e5753dbf4cc9
Gabriel Augendre <gabriel@augendre.info>
2015-05-16 11:29:39 +0200

    Switch to JSliders.

--- src/main/java/aleatoire/MainPanel.java
+++ src/main/java/aleatoire/MainPanel.java
@@ -20,13 +20,13 @@ public class MainPanel {
     private JButton generateButton;
     private JButton endButton;
     private JTextField maxValueField;
-    private JScrollBar minValueScrollBar;
-    private JScrollBar maxValueScrollBar;
+    private JSlider minValueSlider;
+    private JSlider maxValueSlider;
 
     public MainPanel(JFrame container) {
         this.container = container;
-        minValueScrollBar.addAdjustmentListener(e -> minValueField.setText(String.valueOf(e.getValue())));
-        maxValueScrollBar.addAdjustmentListener(e -> maxValueField.setText(String.valueOf(e.getValue())));
+        minValueSlider.addChangeListener(e -> minValueField.setText(String.valueOf(minValueSlider.getValue())));
+        maxValueSlider.addChangeListener(e -> maxValueField.setText(String.valueOf(maxValueSlider.getValue())));
         generateButton.addActionListener(e -> {
             int max = Integer.parseInt(maxValueField.getText());
             int min = Integer.parseInt(minValueField.getText());
@@ -85,13 +85,14 @@ public class MainPanel {
         maxValueField.setFocusable(false);
         maxValueField.setText("100");
         panel1.add(maxValueField, new GridConstraints(0, 5, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, -1), null, 0, false));
-        minValueScrollBar = new JScrollBar();
-        minValueScrollBar.setVisibleAmount(0);
-        panel1.add(minValueScrollBar, new GridConstraints(0, 2, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_VERTICAL, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_WANT_GROW, null, new Dimension(-1, 100), null, 0, false));
-        maxValueScrollBar = new JScrollBar();
-        maxValueScrollBar.setValue(90);
-        maxValueScrollBar.setVisibleAmount(0);
-        panel1.add(maxValueScrollBar, new GridConstraints(0, 6, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_VERTICAL, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_WANT_GROW, null, new Dimension(-1, 100), null, 0, false));
+        minValueSlider = new JSlider();
+        minValueSlider.setOrientation(1);
+        minValueSlider.setValue(0);
+        panel1.add(minValueSlider, new GridConstraints(0, 2, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        maxValueSlider = new JSlider();
+        maxValueSlider.setOrientation(1);
+        maxValueSlider.setValue(100);
+        panel1.add(maxValueSlider, new GridConstraints(0, 6, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         final JPanel panel2 = new JPanel();
         panel2.setLayout(new GridLayoutManager(1, 4, new Insets(0, 0, 0, 0), -1, -1));
         mainPanel.add(panel2, new GridConstraints(5, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
